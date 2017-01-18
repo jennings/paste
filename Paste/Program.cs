24441fcb9dd28bcf19e2856d1e40057818c9aa6e
Stephen Jennings <stephen.g.jennings@gmail.com>
2017-01-17 16:24:00 -0800

    Initial commit

---
+++ Paste/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Paste
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.UnicodeText))
+            {
+                var text = Clipboard.GetText(TextDataFormat.UnicodeText);
+                Console.Out.Write(text);
+                Console.Out.Flush();
+            }
+            else if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                var text = Clipboard.GetText(TextDataFormat.Text);
+                Console.Out.Write(text);
+                Console.Out.Flush();
+            }
+        }
+    }
+}
